4f14480e870935673a8d9cc11437399da13ce0f5
Starkkad <abilevich@itba.edu.ar>
2022-06-20 20:01:54 -0300

    chore lambdas with dynamo

--- terraform/resources/lambda/src/hortz.py
+++ terraform/resources/lambda/src/hortz.py
@@ -1,10 +1,31 @@
-def handler(event, context):
-	resp = {
-		"statusCode": 200,
-		"headers": {
-			"Access-Control-Allow-Origin": "*",
-		},
-		"body": "Hortz >> Hertz"
-	}
+# def handler(event, context):
+# 	resp = {
+# 		"statusCode": 200,
+# 		"headers": {
+# 			"Access-Control-Allow-Origin": "*",
+# 		},
+# 		"body": "Hortz >> Hertz"
+# 	}
 
-	return resp
+# 	return resp
+import boto3, json
+
+def handler(event, context):
+    client = boto3.client('dynamodb')
+    client.put_item(Item= {
+            "userId": {
+                "S": "1"
+            },
+            "username": {
+                "S": "Gabitox3000"
+            }
+        },
+        TableName='users')
+		
+    return {
+        "statusCode": 200,
+        "headers": { 
+            "Access-Control-Allow-Origin": "*", 
+        },
+        "body": "Uploaded user"
+    }
